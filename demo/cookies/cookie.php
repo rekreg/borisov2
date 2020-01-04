466ee9868ac7b907583065b6e0de6ce93315c7cd
Andrey Sychev <rekreg@bk.ru>
2020-01-05 01:16:00 +0300

    Уровень 2: День 1: 0:45:34

--- demo/cookies/cookie.php
+++ demo/cookies/cookie.php
@@ -1,3 +1,11 @@
 <?php
-	setcookie("userName", 'John');
+	//setcookie("userName", 'John');
+	
+	setcookie("userName", 'John', time()-3600);
+	
+	//echo $_COOKIE['userName'];
+
+ 	echo "<pre>";
+	print_r($_COOKIE);
+	echo "</pre>";
 ?>
