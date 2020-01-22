c838af72a59ee5bca01ed03667c306636e783263
Andrey Sychev <rekreg@bk.ru>
2020-01-23 02:00:37 +0300

    Уровень 2: День 3: 0:29:07

--- eshop/inc/config.inc.php
+++ eshop/inc/config.inc.php
@@ -1,1 +1,16 @@
 <?php
+
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "zzxxcc";
+const DB_NAME = "eshop";
+
+const ORDERS_LOG = "orders.log";
+
+$basket = [];
+$count = 0;
+
+
+//echo 1;
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die(mysqli_connect_error());
+
