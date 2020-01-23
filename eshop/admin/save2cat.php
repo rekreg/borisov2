60b88691ba003916431ca805bb71361e5dba282e
Andrey Sychev <rekreg@bk.ru>
2020-01-24 00:25:13 +0300

    Уровень 2: День 3: 0:44:11

--- eshop/admin/save2cat.php
+++ eshop/admin/save2cat.php
@@ -1,5 +1,19 @@
 <?php
 	// подключение библиотек
 	require "secure/session.inc.php";
-	require "../inc/lib.inc.php";
 	require "../inc/config.inc.php";
+	require "../inc/lib.inc.php";
+
+
+$title = clear_string($_POST['title']) ?? "";
+$author = clear_string($_POST['author']) ?? "";
+$pubyear = clear_int($_POST['pubyear']) ?? 0;
+$price = clear_int($_POST['price']) ?? 0;
+
+
+if(!addItemToCatalog($title, $author, $pubyear, $price)): 
+  echo 'Произошла ошибка при добавлении товара в каталог';
+else:
+  header("Location: add2cat.php"); 
+  exit;
+endif;
--- eshop/inc/lib.inc.php
+++ eshop/inc/lib.inc.php
@@ -1,1 +1,37 @@
 <?php
+
+
+
+function clear_string($str){
+	global $link;
+	$str = trim(strip_tags($str));
+	return mysqli_real_escape_string($link, $str);
+}
+
+
+function clear_int($data){
+  return abs((int)$data);
+}
+
+
+function addItemToCatalog($title, $author, $pubyear, $price) {
+  
+  $sql = 'INSERT INTO catalog (title, author, pubyear, price) VALUES (?, ?, ?, ?)';
+  
+  global $link;
+  if (!$stmt = mysqli_prepare($link, $sql)):
+    return false;
+  endif;
+  
+  mysqli_stmt_bind_param($stmt, "ssii", $title, $author, $pubyear, $price);
+  
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  
+  return true;
+  
+}
+
+
+
+
