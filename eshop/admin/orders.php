32f8f0206848ba801a2661a6fda003b3148d7b78
Andrey Sychev <rekreg@bk.ru>
2020-01-29 01:48:42 +0300

    Закончил Уровень 2: День 3

--- eshop/admin/orders.php
+++ eshop/admin/orders.php
@@ -2,6 +2,7 @@
 	require "secure/session.inc.php";
 	require "../inc/lib.inc.php";
 	require "../inc/config.inc.php";
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,17 +13,28 @@
 <body>
 <h1>Поступившие заказы:</h1>
 <?php
-
+$orders = getOrders();
+if(!$orders):
+  echo "Заказов нет";
+  exit;
+endif;
+  
+foreach($orders as $order): 
+  $i = 0; $count = 0;
 ?>
 <hr>
-<h2>Заказ номер: </h2>
-<p><b>Заказчик</b>: </p>
-<p><b>Email</b>: </p>
-<p><b>Телефон</b>: </p>
-<p><b>Адрес доставки</b>: </p>
-<p><b>Дата размещения заказа</b>: </p>
+<h2>Заказ номер: <?=$order["orderid"]?></h2>
+<p><b>Заказчик</b>: <?=$order["name"]?></p>
+<p><b>Email</b>: <?=$order["email"]?></p>
+<p><b>Телефон</b>: <?=$order["phone"]?></p>
+<p><b>Адрес доставки</b>: <?=$order["address"]?></p>
+<p><b>Дата размещения заказа</b>: <?=date("d-m-Y  h:i", (int) $order['date'])?> </p>
+
+
+
 
 <h3>Купленные товары:</h3>
+
 <table border="1" cellpadding="5" cellspacing="0" width="90%">
 <tr>
 	<th>N п/п</th>
@@ -33,9 +45,27 @@
 	<th>Количество</th>
 </tr>
 
+<?php 
+  foreach($order['goods'] as $item):
+?>
+<tr>
+	<td><?= ++$i?></td>
+  <td><?= $item['title']?></td>
+  <td><?= $item['author']?></td>
+  <td><?= $item['pubyear']?></td>
+  <td><?= $item['price']?></td>
+  <td><?= $item['quantity']?></td>
+</tr>
+
+<?php 
+  $count += $item['price'] * $item['quantity'];
+  endforeach;
+?>
 
 </table>
-<p>Всего товаров в заказе на сумму: руб.</p>
-
+<p>Всего товаров в заказе на сумму: <?=$count?> руб.</p>
+<?php
+endforeach;
+?>
 </body>
 </html>
